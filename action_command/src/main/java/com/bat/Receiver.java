62e27e2376777dc664316579c17e39436e7eca53
ZhengYu <1584558751@qq.com>
2019-05-20 10:26:42 +0800

    命令模式（未理解）

---
+++ action_command/src/main/java/com/bat/Receiver.java
@@ -0,0 +1,14 @@
+package com.bat;
+
+/**
+ * @ClassName Receiver
+ * @Description 执行一个与请求相关的操作
+ * @Author ZhengYu
+ * @Version: 1.0
+ * @Create: 2019/5/20 9:26
+ **/
+public class Receiver {
+    public void action() {
+        System.out.println("执行请求!");
+    }
+}
