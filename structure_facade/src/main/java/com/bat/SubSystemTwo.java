9cd5f4af79fe8a3fde7c50283dea4b817ed210fa
ZhengYu <1584558751@qq.com>
2019-05-28 10:01:58 +0800

    外观模式

---
+++ structure_facade/src/main/java/com/bat/SubSystemTwo.java
@@ -0,0 +1,15 @@
+package com.bat;
+
+/**
+ * @ClassName SubSystemOne
+ * @Description 实现子系统的功能，处理 Facade 对象指派的任务，注意子类中没有 Facade 的任何信息
+ * @Author ZhengYu
+ * @Version: 1.0
+ * @Create: 2019/5/28 9:16
+ **/
+public class SubSystemTwo {
+
+    public void method(){
+        System.out.println("SubSystemTwo ... method()");
+    }
+}
