0d5df9c90129bf360d412adca5c17749aa23512d
ZhengYu <1584558751@qq.com>
2019-05-30 09:46:58 +0800

    组合模式

---
+++ structure_composite/src/main/java/com/bat/Leaf.java
@@ -0,0 +1,46 @@
+package com.bat;
+
+/**
+ * @ClassName Leaf
+ * @Description 在组合中表示叶节点对象，叶节点没有子节点
+ * @Author ZhengYu
+ * @Version: 1.0
+ * @Create: 2019/5/30 9:22
+ **/
+public class Leaf extends Component {
+
+    public Leaf(String name) {
+        super(name);
+    }
+
+    /**
+     * @param component
+     * @Param [component]
+     * @Return void
+     * @Author ZhengYu
+     * @Description: 通常用来提供增加树枝的功能
+     * @Date 2019/5/30
+     */
+    @Override
+    public void add(Component component) {
+        System.out.println("Cannot add to leaf");
+    }
+
+    /**
+     * @param component
+     * @Param [component]
+     * @Return void
+     * @Author ZhengYu
+     * @Description: 通常用来提供移除树枝的功能
+     * @Date 2019/5/30
+     */
+    @Override
+    public void remove(Component component) {
+        System.out.println("Cannot remove from a leaf");
+    }
+
+    @Override
+    public void display(int depth) {
+        System.out.println(depth + " - " + name);
+    }
+}
