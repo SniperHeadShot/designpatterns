35659e2547cb4613a812b0e94850bbe7d3134107
ZhengYu <1584558751@qq.com>
2019-05-31 09:55:25 +0800

    享元模式

---
+++ structure_flyweight/src/main/java/com/bat/Flyweight.java
@@ -0,0 +1,21 @@
+package com.bat;
+
+/**
+ * @ClassName Flyweight
+ * @Description 所有享元类的超类或者接口，通过这个接口，Flyweight 可以接受并作用于外部状态
+ * @Author ZhengYu
+ * @Version: 1.0
+ * @Create: 2019/5/31 9:23
+ **/
+public abstract class Flyweight {
+
+    /**
+     * @Param []
+     * @Return void
+     * @Author ZhengYu
+     * @Description: 所有享元类的接口
+     * @Date 2019/5/31
+     */
+    public abstract void operation(int extrinsicState);
+
+}
---
+++ structure_flyweight/src/main/java/com/bat/FlyweightFactory.java
@@ -0,0 +1,33 @@
+package com.bat;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+/**
+ * @ClassName FlyweightFactory
+ * @Description 享元工厂，用来创建并管理 Flyweight 对象，主要是用来确保合理地共享 Flyweight
+ * @Author ZhengYu
+ * @Version: 1.0
+ * @Create: 2019/5/31 9:33
+ **/
+public class FlyweightFactory {
+
+    private Map<String, Flyweight> flyweights = new Hashtable<>();
+
+    public FlyweightFactory() {
+        flyweights.put("X", new ConcreteFlyweight());
+        flyweights.put("Y", new ConcreteFlyweight());
+        flyweights.put("Z", new ConcreteFlyweight());
+    }
+
+    /**
+     * @Param [key]
+     * @Return com.bat.Flyweight
+     * @Author ZhengYu
+     * @Description: 根据客户端请求，获得已生成的实例
+     * @Date 2019/5/31
+     */
+    public Flyweight getFlyweight(String key) {
+        return flyweights.get(key);
+    }
+}
