3ba57c5d2f8c8d05451bdc1ff91e0a75aa57244a
ZhengYu <1584558751@qq.com>
2019-08-03 18:15:42 +0800

    代理模式

---
+++ structure_proxy/src/main/java/com/bat/jdkproxy/JdkProxy.java
@@ -0,0 +1,11 @@
+package com.bat.jdkproxy;
+
+/**
+ * JDK动态代理
+ *
+ * @author ZhengYu
+ * @version 1.0 2019/8/3 18:05
+ **/
+public class JdkProxy {
+    // 参考 https://www.jianshu.com/p/269afd0a52e6
+}
