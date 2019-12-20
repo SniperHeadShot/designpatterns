72eb32c4d0576bf1beaddd704b6de28929c97c17
ZhengYu <zhengy@gato.cn>
2019-12-20 14:04:13 +0800

    策略模式

---
+++ $02strategy/src/main/java/com/bat/strategy/StrategyClient.java
@@ -0,0 +1,13 @@
+package com.bat.strategy;
+
+/**
+ * Strategy 策略模式
+ *
+ * @author ZhengYu
+ * @version 1.0 2019/12/20 13:32
+ **/
+public class StrategyClient {
+    public static void main(String[] args) {
+
+    }
+}
